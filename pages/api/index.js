c6f6016b0d8b17125fb1d5048bcdae1dde917a4e
amit151575 <amitydv1121@gmail.com>
2025-09-04 16:26:33 +0530

    Delete index.js

--- pages/api/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{textAlign: "center", marginTop: "50px"}}>
-      <h1>🏫 School App</h1>
-      <p><a href="/addSchool">➕ Add a School</a></p>
-      <p><a href="/showSchools">📚 View Schools</a></p>
-    </div>
-  );
-}
